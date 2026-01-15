04e0999de76b1b4bfee18b0de1c101e46b32e7f2
Emily <emilys@genstudents.org>
2026-01-15 15:56:07 -0300

    :sparkles: Creating the database

--- src/middlewares/routeHandler.js
+++ src/middlewares/routeHandler.js
@@ -1,10 +1,13 @@
 import { routes } from '../routes/index.js';
+import { Database } from '../database/database.js';
+
+const database = new Database();
 
 export function routeHandler(request, response) {
     const route = routes.find(route => route.method === request.method && route.path === request.url);
 
     if (route) {
-        route.controller({request, response});
+        route.controller({request, response, database});
     } else {
         response.writeHead(404, { 'Content-Type': 'application/json' });
         response.end(JSON.stringify({ message: '❌ Route not found' }));
