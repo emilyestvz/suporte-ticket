069700ca0ac403965301805738c3fde751e81678
Emily <emilys@genstudents.org>
2026-01-19 14:52:06 -0300

    :fire: Adding the solution to the ticket and removing a ticket

--- src/controllers/tickets/updateStatus.js
+++ src/controllers/tickets/updateStatus.js
@@ -1,7 +1,9 @@
 export function updateStatus({request, response, database}) {
 
     const { id } = request.params;
-    database.update('tickets', id, { status: 'closed' });
+    const { solution } = request.body;
+
+    database.update('tickets', id, { status: 'closed', solution, closed_at: new Date() });
 
     return response.end(JSON.stringify({
         message: '✅ Ticket closed successfully',
