6aa758f949883a296bee4001771be87d2aeb5668
Emily <emilys@genstudents.org>
2026-01-19 14:17:04 -0300

    :sparkles: Route to update ticket status

--- src/controllers/tickets/updateStatus.js
+++ src/controllers/tickets/updateStatus.js
@@ -1,3 +1,9 @@
 export function updateStatus({request, response, database}) {
-    return response.end('ok');
+
+    const { id } = request.params;
+    database.update('tickets', id, { status: 'closed' });
+
+    return response.end(JSON.stringify({
+        message: '✅ Ticket closed successfully',
+    }));
 }
